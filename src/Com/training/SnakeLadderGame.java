4d90be4a25d69fe54b6fe06ccbf4090fbd0d1361
Yudha13Koley <yudhajit1998@gmail.com>
2020-09-22 21:29:28 +0530

    UC4 done

--- src/Com/training/SnakeLadderGame.java
+++ src/Com/training/SnakeLadderGame.java
@@ -4,8 +4,10 @@ public class SnakeLadderGame {
 	public static final int No_Play=0;
 	public static final int Ladder=1;
 	public static final int Snake=2;
+	public static final int Winning_Position=100;
 	public static void main(String[] args) {
 		int PlayerPosition=0,turn=0;
+		while(PlayerPosition< Winning_Position) {
 		int DieRoll =(int)Math.floor(Math.random()*10)%6 +1;
 		int CheckOptions=(int)Math.floor(Math.random()*10)%3;
 		switch(CheckOptions)
@@ -17,7 +19,10 @@ public class SnakeLadderGame {
 						break;
 		default : 		break;
 		}
+		if(PlayerPosition<0)
+			PlayerPosition=0;
 		System.out.println("The position is : "+PlayerPosition);
 	}
 
+}
 }
