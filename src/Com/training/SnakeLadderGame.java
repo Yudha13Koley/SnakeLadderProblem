2a853039953d27312a5b49e38ed6a5c99d20437d
Yudha13Koley <yudhajit1998@gmail.com>
2020-09-22 21:22:41 +0530

    UC3 done

--- src/Com/training/SnakeLadderGame.java
+++ src/Com/training/SnakeLadderGame.java
@@ -1,9 +1,23 @@
 package Com.training;
 
 public class SnakeLadderGame {
+	public static final int No_Play=0;
+	public static final int Ladder=1;
+	public static final int Snake=2;
 	public static void main(String[] args) {
 		int PlayerPosition=0,turn=0;
 		int DieRoll =(int)Math.floor(Math.random()*10)%6 +1;
+		int CheckOptions=(int)Math.floor(Math.random()*10)%3;
+		switch(CheckOptions)
+		{
+		case No_Play : 	break ;
+		case Ladder : PlayerPosition+=DieRoll;
+						break;
+		case Snake : PlayerPosition-=DieRoll;
+						break;
+		default : 		break;
+		}
+		System.out.println("The position is : "+PlayerPosition);
 	}
 
 }
