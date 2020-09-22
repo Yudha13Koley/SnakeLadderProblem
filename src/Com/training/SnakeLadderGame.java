dce0aa7aee9d957947f9b09dbe71bbfde956d339
Yudha13Koley <yudhajit1998@gmail.com>
2020-09-22 21:16:57 +0530

    UC2 done

--- src/Com/training/SnakeLadderGame.java
+++ src/Com/training/SnakeLadderGame.java
@@ -3,6 +3,7 @@ package Com.training;
 public class SnakeLadderGame {
 	public static void main(String[] args) {
 		int PlayerPosition=0,turn=0;
+		int DieRoll =(int)Math.floor(Math.random()*10)%6 +1;
 	}
 
 }
